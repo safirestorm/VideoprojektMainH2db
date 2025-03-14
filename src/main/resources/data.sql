fe8dda01badce2f0e93ce8ce94bfcfa766d28302
Claus Bové <clbo@kea.dk>
2025-03-14 10:45:15 +0100

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO cars (brand, modelyear, type, colour, licenseplate, img) VALUES
+    ('Volvo', 2012, 'Sedan', 'Red', 'ABC123', 'blåvolvostationcar.jpg'),
+    ('Ford', 1938, 'SUV', 'Blue', 'DEF456', 'generic.jpg'),
+    ('VW', 1986, 'Coupe', 'Black', 'GHI789', 'GulVWBobbel.jpg'),
+    ('Western Star', 1998, 'Hatchback', 'White', 'JKL012', 'WesternstarRødTruck.jpg'),
+    ('Hyundai', 2002, 'Convertible', 'Silver', 'MNO345', 'generic.jpg');
